c5f69a67ab4ca8477826bd121bd7a0f5ad2c96a3
Elakiya-Hub <elakiya@DESKTOP-D2J8OLM.localdomain>
2026-07-01 05:12:24 +0000

    Local reo is created and pushed to remote repo

---
+++ demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "========================"
+echo "Welcome to Jenkins CI"
+echo "Current Date:"
+date
+
+echo "Current User:"
+whoami
+
+echo "Host Name:"
+hostname
+
+echo "Current Directory:"
+pwd
+
+echo "========================"
